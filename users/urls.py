20823977881894f3ba0128cc69eea061510148cd
deda <ded@>
2024-08-21 18:13:47 +0300

    Создание метода отображения всех пользователей через GET запрос в POSTMAN.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, UserList
 
 urlpatterns = [
     path('create_user/', CreateUserView.as_view(), name='create_user'),
+    path('users/', UserList.as_view(), name='user-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import UserSerializer
@@ -14,3 +14,8 @@ class CreateUserView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
