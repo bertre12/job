ca97b91a2d653a50dcd9aceff5bc90caa263998c
deda <ded@>
2024-08-30 11:15:11 +0300

    Создание файла forms.py, где описывается логика отображения полей из бд и для внесения изменений. Создание метода в файле views.py для редактирования и сохранения данных в бд. Создание наглядного шаблона edit_student.html для вывода изменяемых данных.

--- student/views.py
+++ student/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from student.forms import EditForm
 from student.models import Student
 
 
+# Отображение данных студента.
 def your_student(request):
     data = Student.objects.all()
     return render(request, 'student/student.html', {'data': data})
+
+
+# Изменение данных студента.
+def edit_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if request.method == 'POST':
+        form = EditForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student')  # Перенаправление на другую страницу, после изменений данных.
+    else:
+        form = EditForm(instance=student)
+    return render(request, 'student/edit_student.html', {'form': form})
